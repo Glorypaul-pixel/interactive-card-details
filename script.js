2690928419a6c0c63e3db0341de89dbc5018753c
Glorypaul-pixel <81073279+Glorypaul-pixel@users.noreply.github.com>
2022-09-26 17:22:12 +0100

    second commit

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ const cardNumberBox = document.querySelector(".card-number");
 const cardNameBox = document.querySelector(".card-name");
 const monthBox = document.querySelector(".month");
 const yearBox = document.querySelector(".year");
-const cvcBox = document.getElementById (".cvc");
+const cvcBox = document.getElementById(".cvc");
 const cardHolderNameInput = document.querySelector(".cardholder-name");
 const cardNumberInput = document.querySelector(".card-number");
-const  
